49aa30c9d2e838c68ffca6f755b4b2c7f8d46240
vi2hnu <vishnuvaradhankr@gmail.com>
2025-11-17 19:07:57 +0530

    fixed recursive results

--- src/main/java/org/example/booking/model/entity/Passenger.java
+++ src/main/java/org/example/booking/model/entity/Passenger.java
@@ -1,10 +1,20 @@
 package org.example.booking.model.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 import org.example.booking.model.enums.Gender;
 import org.example.booking.model.enums.Meal;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Setter
 @Getter
@@ -26,6 +36,7 @@ public class Passenger {
 
     @ManyToOne
     @JoinColumn(name = "ticket_id")
+    @JsonBackReference
     private Ticket ticket;
 
 }
--- src/main/java/org/example/booking/model/entity/Ticket.java
+++ src/main/java/org/example/booking/model/entity/Ticket.java
@@ -1,12 +1,23 @@
 package org.example.booking.model.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.example.booking.model.enums.Status;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.example.booking.model.enums.Status;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Setter
 @Getter
@@ -28,6 +39,7 @@ public class Ticket {
     private Schedule returnTrip;
 
     @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
+    @JsonManagedReference
     private List<Passenger> passengers = new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
